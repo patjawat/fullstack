d8de2db08b268d68f3563e4e12b253fda5d82bed
patjawat <patjawat@gmail.com>
2020-04-15 15:14:40 +0700

    update

--- car2Pro/yii2app/vendor/coderius/yii2-hit-counter/src/config/_routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    
-    'counter' => 'hit-counter/index',
-    'counter/<action>' => 'hit-counter/<action>',
-    
-    // 'GET api/default/comments-tree/<entity:\w+>/<entityId:\d+>/<pageNum:\d+>' => 'default/comments-tree',
-];
