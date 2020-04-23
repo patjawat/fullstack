fab7e60a84c05775cbd7e6ab2e711cae54256072
ปัจวัฒน์ ศรีบุญเรือง <patjawat@Macintosh.local>
2020-04-23 11:00:08 +0700

    update

--- reactapp/src/pages/patient/PatientForm.js
+++ reactapp/src/pages/patient/PatientForm.js
@@ -1,23 +1,50 @@
 import React, { Component, useState } from 'react';
+import { useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+
+
 
 const PatientForm = (props) => {
-    const initialFormState = {  }
+
+    const ADD_TODO = gql`
+    mutation createPatient($fullname: String!) {
+        createPatient(fullname:$fullname ) {
+        fullname
+      }
+    }
+  `;
+
+  const EXCHANGE_RATES = gql`
+{
+    allPatient{
+        id
+      fullname
+    }
+  }
+`;
+
+
+    const initialFormState = {}
     const [patient, setPatient] = useState(initialFormState)
+    const [addTodo, { data }] = useMutation(ADD_TODO);
+    const [updateTodo] = useMutation(UPDATE_TODO);
+
     const handleInputChange = e => {
         const { name, value } = e.target
         setPatient({ ...patient, [name]: value })
-    }
-    const savePAtient = e => {
-        e.preventDefault()
-
-        console.log(patient)
-        setPatient({})
     }
 
     return (
         <div className="mb-3">
             {JSON.stringify(patient)}
-            <form onSubmit={savePAtient}>
+            <form
+                onSubmit={e => {
+                    e.preventDefault();
+                    addTodo({ variables: patient });
+                    patient:{}
+                }}
+            >
                 <div className="form-group">
                     <label for="">ชื่อ-นามสกุล</label>
                     <input type="text" name="fullname" className="form-control" placeholder="ระบุชื่อ-นามสกุล" onChange={handleInputChange} />
