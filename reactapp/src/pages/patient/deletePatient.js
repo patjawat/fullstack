33e1fb86860e57ff5c089f6ed25abd7b0c735f4d
ปัจวัฒน์ ศรีบุญเรือง <patjawat@Macintosh.local>
2020-04-25 10:49:18 +0700

    update

--- reactapp/src/pages/patient/deletePatient.js
+++ reactapp/src/pages/patient/deletePatient.js
@@ -3,34 +3,70 @@ import { Button } from 'reactstrap';
 import { Mutation } from "react-apollo";
 import { DELETE_PATIENT } from "../../gql/mutation/patient";
 import { ALL_PATIENT } from "../../gql/query/";
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
-class DeletePatient extends React.Component {
-  render() {
-    const { id } = this.props;
-    return (
-      <Mutation
-        mutation={DELETE_PATIENT}
-        onError={(response) => {
-          console.log(response);
-        }}
-      >
-        {(deletePatient) => (
+const MySwal = withReactContent(Swal)
 
-          <Button color="danger"
+
+const DeletePatient = (props) => {
+  const id = props.id;
+  return (
+    <Mutation
+      mutation={DELETE_PATIENT}
+      onError={(response) => {
+        console.log(response);
+      }}>
+      {(deletePatient) => (
+        <Button color="danger"
           onClick={(e) => {
             e.preventDefault();
-            deletePatient({
-              variables: { id: id },
-              refetchQueries: [{ query: ALL_PATIENT }],
-            });
-          }}
-          >Delete</Button>
+            MySwal.fire({
+              title: 'Are you sure?',
+              text: "You won't be able to revert this!",
+              icon: 'warning',
+              showCancelButton: true,
+              confirmButtonColor: '#3085d6',
+              cancelButtonColor: '#d33',
+              confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+              if (result.value) {
+                MySwal.fire(
+                  'Deleted!',
+                  'Your file has been deleted.',
+                  'success'
+                )
+                deletePatient({
+                  variables: { id: id },
+                  refetchQueries: [{ query: ALL_PATIENT }],
+                });
+              }
+            })
            
-       
-        )}
-      </Mutation>
-    );
-  }
+          }}>Delete</Button>
+      )}
+    </Mutation>
+  );
+}
+
+const confirmDelete = () => {
+  MySwal.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then((result) => {
+    if (result.value) {
+      MySwal.fire(
+        'Deleted!',
+        'Your file has been deleted.',
+        'success'
+      )
+    }
+  })
 }
 
 export default DeletePatient;
