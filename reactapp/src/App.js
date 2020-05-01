4377102a67f57e83cf4b421fea7439a5cb707980
ปัจวัฒน์ ศรีบุญเรือง <patjawat@Macintosh.local>
2020-05-01 19:31:57 +0700

    update

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,115 +1,16 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-  useHistory,
-  useLocation
-} from "react-router-dom";
-import { useMutation } from "@apollo/react-hooks";
-import Header from './components/Header'
-import Cart from './components/cart'
-import Home from '././components/home'
-import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFingerprint } from '@fortawesome/free-solid-svg-icons'
-import {CounterProvider} from './hooks'
+import React, {useContext} from "react";
 
-export default function App() {
-  return (
-<CounterProvider>
+import { AuthContext } from './context/Auth.context.js';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
-      <div className="containers">
-        <div className="app-wrapperx">
-          <Router>
-            <AuthButton />
-            <div className="container mt-5 p-5 bg-white">
-              <Switch>
-                <PrivateRoute path="/cart">
-                  <Cart />
-                </PrivateRoute>
-                <PrivateRoute path="/">
-                  <Home />
-                </PrivateRoute>
-              </Switch>
-            </div>
-          </Router>
-        </div>
-      </div>
-</CounterProvider>
+const App = () => {
+  const { state } = useContext(AuthContext);
 
-  );
+  if (!state.isLoggedIn) 
+    return <Login />;
+  else
+    return <Dashboard />
 }
 
-const fakeAuth = {
-  isAuthenticated: true,
-  authenticate(cb) {
-    fakeAuth.isAuthenticated = true;
-    setTimeout(cb, 100); // fake async
-  },
-  signout(cb) {
-    fakeAuth.isAuthenticated = false;
-    setTimeout(cb, 100);
-  }
-};
-
-function AuthButton() {
-  let history = useHistory();
-  return fakeAuth.isAuthenticated ? (
-    <>
-    <Header />
-    </>
-  ) : (
-      <LoginPage />
-    );
-}
-
-function PrivateRoute({ children, ...rest }) {
-  return (
-    <Route
-      {...rest}
-      render={({ location }) =>
-        fakeAuth.isAuthenticated ? (
-          children
-        ) : (
-            <Redirect
-              to={{
-                pathname: "/login",
-                state: { from: location }
-              }}
-            />
-          )
-      }
-    />
-  );
-}
-
-const  LoginPage = ()=> {
-  return (
-    <div>
-      <div className="card text-white bg-dark shadow-lg rounded wrapper-box" style={{ width:'30rem',maxWidth: '18rem;' }}>
-        <div className="card-header">
-          <h5><FontAwesomeIcon icon={faFingerprint} /> Authentication</h5>
-          </div>
-        <div className="card-body">
-    <div class="form-group">
-      <label for="email">Username:</label>
-      <input type="email" class="form-dark" id="email" placeholder="Enter email" name="email"/>
-    </div>
-    <div class="form-group">
-      <label for="pwd">Password:</label>
-      <input type="password" class="form-dark" id="pwd" placeholder="Enter password" name="pswd" />
-    </div>
-    <div class="form-group form-check">
-      <label class="form-check-label">
-        <input class="form-check-input" type="checkbox" name="remember" /> Remember me
-      </label>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-
-        </div>
-      </div>
-    </div>
-  );
-}
+export default App;
